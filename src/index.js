ec3fee6beeedb68c2bb0e153e94f887f2b949406
Saulo Joab <saulojoabps@gmail.com>
2019-12-05 21:14:39 -0300

    implementing navigation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import Page1 from './pages/page1';
+import Page2 from './pages/page2';
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact component={Page1} />
+            <Route path="/page2" component={Page2} />
+        </Switch>
+    </BrowserRouter>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/pages/page2.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Page2(){
+    return (
+        <>
+            <h1>OK PAGE 2</h1>
+        </>
+    )
+}
